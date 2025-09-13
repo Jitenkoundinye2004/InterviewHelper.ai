511a14693d1c007c27ce85bbdf016aec60fc15d8
Jiten Koundinye <jitenkoundinyecse@gmail.com>
2025-09-13 23:59:02 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,10 +1,12 @@
 
 
 
+
 // src/utils/axiosInstance.js
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "https://interviewhelper-ai.onrender.com";
+// const baseURL = import.meta.env.VITE_API_URL || "https://interviewhelper-ai.onrender.com";
+const baseURL = import.meta.env?.VITE_API_URL || "https://your-backend-url.onrender.com" || "http://localhost:8000";
 
 const axiosInstance = axios.create({
   baseURL,
